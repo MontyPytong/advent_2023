4a01e36a6c9cc64b6b39226935beb5446c2ac0fb
Cristi Ene <ene.georgec@gmail.com>
2023-12-03 02:21:46 +0200

    solved advent day 1

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::{
     fmt::format,
     fs::File,
@@ -11,26 +12,63 @@ fn main() {
 }
 
 fn read_file_line_by_line(filepath: &str) -> i32 {
-    let mut add: i32 = 0;
+    let number = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut sum: i32 = 0;
+    let mut count: i32 = 0;
+    let mut add_count: i32 = 0;
+
     let file = File::open(filepath).unwrap();
     let reader = BufReader::new(file);
     let _ = for line in reader.lines() {
+        println!("[ {} ]+++++++++++++++++++++++++++++++++", count);
+        count += 1;
         for char in line {
-            let v: Vec<&str> = char.matches(char::is_numeric).collect();
-            //println!("v : {:?}, v.len : {:?}", v, v.len());
+            let mut i: u32 = 0;
+            let mut value: String = String::from(char.clone());
+            println!("original : {:?}", char);
+
+            for n in number {
+                let g_index: Option<usize> = value.find("oneight");
+
+                if value.as_str().contains("oneight") {
+                    value = value.replace("oneight", 18.to_string().as_str());
+                }
+                if value.as_str().contains("twone") {
+                    value = value.replace("twone", 21.to_string().as_str());
+                }
+
+                if value.as_str().contains("eightwo") {
+                    value = value.replace("eightwo", 82.to_string().as_str());
+                }
+
+                if value.as_str().contains(n) {
+                    value = value.replace(n, i.to_string().as_str());
+                };
+                i += 1;
+            }
+            println!("transformed : {:?}", value);
+
+            let v: Vec<&str> = value.matches(char::is_numeric).collect();
 
             if v.len() < 2 {
                 let first = v.first().unwrap().parse::<i32>().unwrap();
                 let last = v.first().unwrap().parse::<i32>().unwrap();
-                let sum = format!("{}{}", first, last).parse::<i32>().unwrap();
-                add += sum;
+                let combine = format!("{}{}", first, last).parse::<i32>().unwrap();
+                println!("mai avem caz dasta {:?}", combine);
+                sum += combine;
+                add_count += 1;
             } else {
                 let first = v.first().unwrap().parse::<i32>().unwrap();
                 let last = v.last().unwrap().parse::<i32>().unwrap();
-                let sum = format!("{}{}", first, last).parse::<i32>().unwrap();
-                add += sum;
+                let combine = format!("{}{}", first, last).parse::<i32>().unwrap();
+                println!("{:?}", combine);
+                sum += combine;
+                add_count += 1;
             }
+            println!("total : {:?}", sum);
         }
     };
-    return add;
+    return sum;
 }
